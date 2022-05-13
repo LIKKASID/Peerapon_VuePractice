b79be300f6a4f082ed06eed14d994d0eb31d77f3
LIKKASID <92563951+LIKKASID@users.noreply.github.com>
2022-05-13 15:58:57 +0700

    Initial commit

---
+++ src/components/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <card></card>
+    <card>2nd Card</card>
+    <card
+      ><card><card>test</card></card></card
+    >
+  </div>
+</template>
+
+<script>
+import card from "./card";
+export default {
+  components: {
+    card,
+  },
+};
+</script>
